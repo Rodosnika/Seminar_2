61a1b14652b40548ec75e7575183ec6e46761dc3
Veronika <vvnika@mail.ru>
2022-07-06 02:15:10 +1100

    add "Доманее задание Семинар_2, Задача 15"

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ int max = Max(max1, max2, max3);
 
 Console.WriteLine(max1);
 */
+/*
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
@@ -42,3 +43,29 @@ else
 {
     Console.WriteLine("-> третьей цифры нет");
 }
+*/
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("(этот день выходной) -> да");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("это вообще не день недели");
+    }
+    else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
